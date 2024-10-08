b33b2ff9df46110b91f4d6c4097746c082147a71
Juanca <juanca.jcglp26@gmail.com>
2024-10-08 20:04:55 +0200

    v1.3

--- 1. Pokemon/index.js
+++ 1. Pokemon/index.js
@@ -1,15 +1,8 @@
-//Librerias importadas
 const readline = require('readline-sync');
 
-
-
-
 // Clases importadas
-const pokemon=require('./pokemon');
-const move=require('./move');
-
-
-
+const pokemon = require('./pokemon');
+const move = require('./move');
 
 // Definiendo algunos movimientos con su daño base
 const impactrueno = new move("Impactrueno", 40);
@@ -17,15 +10,15 @@ const placaje = new move("Placaje", 40);
 const llamarada = new move("Llamarada", 90);
 const ascuas = new move("Ascuas", 40);
 const latigazo = new move("Latigazo", 45);
-const polvoVeneno = new move("Polvo Veneno", 0);  // No hace daño directo
+const polvoVeneno = new move("Polvo Veneno", 0);
 const pistolaAgua = new move("Pistola Agua", 40);
 const burbuja = new move("Burbuja", 40);
-const canto = new move("Canto", 0);  // No hace daño, pero puede dormir
+const canto = new move("Canto", 0);
 const dobleBofetón = new move("Doble Bofetón", 15);
 const bolaSombra = new move("Bola Sombra", 80);
-const hipnosis = new move("Hipnosis", 0);  
+const hipnosis = new move("Hipnosis", 0);
 
-//Lista de Pokemones
+// Lista de Pokemones
 const pokemonList = [
     new pokemon("Pikachu", "Eléctrico", 110, 110, 55, 40, [impactrueno, placaje]),
     new pokemon("Charmander", "Fuego", 120, 120, 52, 43, [llamarada, ascuas]),
@@ -36,21 +29,21 @@ const pokemonList = [
     new pokemon("Eevee", "Normal", 135, 135, 55, 50, [placaje, dobleBofetón])
 ];
 
-//Funcion para despedir programa
+// Función para despedir programa
 function salir() {
-    console.log("Gracias por jugar. ¡Hasta luego!, @juancaaa11");
+    console.log("Gracias por jugar. ¡Hasta luego!");
 }
 
-//Funcion Menu principal
+// Función menú principal
 function mostrarMenu() {
-    console.log("--------------------------------------")
+    console.log("--------------------------------------");
     console.log("Bienvenido al Pokémon Game en consola");
-    console.log("--------------------------------------")
+    console.log("--------------------------------------");
     console.log("1. Iniciar Juego");
     console.log("2. Salir");
-    console.log("--------------------------------------")
+    console.log("--------------------------------------");
     
-    const opcion = readline.question("Ingresa una opcion: ");
+    const opcion = readline.question("Ingresa una opción: ");
     switch (opcion) {
         case '1':
             iniciarJuego();
@@ -64,67 +57,61 @@ function mostrarMenu() {
     }
 }
 
+// Función para elegir Pokémon aleatorio
 function elegirPokemonAleatorio() {
     const randomIndex = Math.floor(Math.random() * pokemonList.length);
     return pokemonList[randomIndex];
 }
 
+// Función para iniciar el juego
 function iniciarJuego() {
     const jugadorPokemon = elegirPokemonAleatorio();
     const oponentePokemon = elegirPokemonAleatorio();
-    
+
     console.log(`Tu Pokémon: ${jugadorPokemon.nombre}`);
     console.log(`Pokémon Oponente: ${oponentePokemon.nombre}`);
-    
-    let turno = 1;
 
-    const batalla = () => {
-        console.log(`\n--- Turno ${turno} ---`);
-        console.log(`${jugadorPokemon.nombre} tiene ${jugadorPokemon.HPactual}/${jugadorPokemon.HPmaximo} HP`);
-        console.log(`${oponentePokemon.nombre} tiene ${oponentePokemon.HPactual}/${oponentePokemon.HPmaximo} HP`);
+    while (jugadorPokemon.HPactual > 0 && oponentePokemon.HPactual > 0) {
+        console.log("\n--- Estado Actual ---");
+        console.log(`${jugadorPokemon.nombre} tiene ${jugadorPokemon.HPactual}/${jugadorPokemon.HPmaximo} HP.`);
+        console.log(`${oponentePokemon.nombre} tiene ${oponentePokemon.HPactual}/${oponentePokemon.HPmaximo} HP.`);
 
-        rl.question("Elige una acción: 1 - Atacar, 2 - Curar: ", (accion) => {
-            if (accion === '1') {
-                rl.question("Elige un movimiento: 0 - " + jugadorPokemon.moves[0].nombre + ", 1 - " + jugadorPokemon.moves[1].nombre + ": ", (moveIndex) => {
-                    jugadorPokemon.atacar(moveIndex, oponentePokemon);
-                    if (oponentePokemon.HPactual > 0) {
-                        // Acción del oponente aleatoria
-                        const accionOponente = Math.random() < 0.5 ? 'atacar' : 'curar'; // 50% de probabilidad para cada acción
-                        if (accionOponente === 'atacar') {
-                            const moveIndexOponente = Math.floor(Math.random() * oponentePokemon.moves.length);
-                            oponentePokemon.atacar(moveIndexOponente, jugadorPokemon);
-                        } else {
-                            oponentePokemon.curar();
-                        }
-                    }
-                    if (jugadorPokemon.HPactual <= 0 || oponentePokemon.HPactual <= 0) {
-                        if (jugadorPokemon.HPactual <= 0) {
-                            console.log("¡Has sido derrotado!");
-                        } else {
-                            console.log("¡Has derrotado a tu oponente!");
-                        }
-                        rl.close(); // Cerrar la interfaz de readline al finalizar la batalla
-                    } else {
-                        turno++;
-                        batalla(); // Continuar con el siguiente turno
-                    }
+        console.log("\n1. Atacar - 2. Curarse - 3. Huir");
+        const accion = readline.question("Ingresa una acción: ");
+
+        switch (parseInt(accion)) {
+            case 1:
+                // Elegir ataque
+                console.log("\nElige un movimiento:");
+                jugadorPokemon.move.forEach((move, index) => {
+                    console.log(`${index + 1}: ${move.nombre}`);
                 });
-            } else if (accion === '2') {
-                jugadorPokemon.curar();
-                turno++;
-                batalla(); // Continuar con el siguiente turno
-            } else {
-                console.log("Acción no válida. Turno perdido.");
-                turno++;
-                batalla(); // Continuar con el siguiente turno
-            }
-        });
-    };
+                const moveIndex = readline.question("Selecciona el movimiento: ") - 1;
+                jugadorPokemon.atacar(moveIndex, oponentePokemon);
+                break;
+            case 2:
+                jugadorPokemon.curarse();
+                break;
+            case 3:
+                console.log("Has huido del combate.");
+                return;
+            default:
+                console.log("Opción no válida.");
+        }
+
+        // Turno de la IA
+        if (oponentePokemon.HPactual > 0) {
+            const randomMoveIndex = Math.floor(Math.random() * oponentePokemon.move.length);
+            oponentePokemon.atacar(randomMoveIndex, jugadorPokemon);
+        }
+    }
+
+    if (jugadorPokemon.HPactual <= 0) {
+        console.log(`Has sido derrotado por ${oponentePokemon.nombre}.`);
+    } else if (oponentePokemon.HPactual <= 0) {
+        console.log(`¡Has derrotado a ${oponentePokemon.nombre}!`);
+    }
 }
 
-
-
-
-
+// Iniciar el programa
 mostrarMenu();
-
--- 1. Pokemon/pokemon.js
+++ 1. Pokemon/pokemon.js
@@ -1,9 +1,7 @@
-const move=require('./move');
+const move = require('./move');
 
 class pokemon {
-
-    constructor(nombre,tipo,HPactual,HPmaximo,ataque,defensa,move){
-
+    constructor(nombre, tipo, HPactual, HPmaximo, ataque, defensa, move) {
         this.nombre = nombre;
         this.tipo = tipo;
         this.HPactual = HPactual;
@@ -11,21 +9,16 @@ class pokemon {
         this.ataque = ataque;
         this.defensa = defensa;
         this.move = move;
-        this.curacion = false;
+        this.puedeCurarse = true; // Puede curarse una vez por combate
     }
 
-
-    /**
-     * 
-     */
     atacar(moveIndex, oponente) {
         const move = this.move[moveIndex];
-        // Cálculo del daño usando la fórmula proporcionada
-        const randomFactor = 0.85 + Math.random() * 0.15; // Random entre 0.85 y 1.0
-        const damage = Math.max(0, (this.ataque / oponente.defensa) * move.danoB * randomFactor);
+        const randomFactor = 0.85 + Math.random() * 0.15;
+        const damage = Math.max(1, (this.ataque / oponente.defensa) * move.baseDamage * randomFactor);
         oponente.HPactual = Math.max(oponente.HPactual - damage, 0);
-        console.log(`${this.nombre} usa ${move.nombre} y causa ${Math.round(damage)} puntos de daño a ${oponente.nombre}`);
-        
+        console.log(`${this.nombre} usó ${move.nombre} y causó ${Math.round(damage)} puntos de daño a ${oponente.nombre}.`);
+
         if (oponente.HPactual <= 0) {
             console.log(`${oponente.nombre} ha sido derrotado.`);
         } else {
@@ -33,20 +26,16 @@ class pokemon {
         }
     }
 
-    /**
-     * 
-     */
-    curar() {
+    curarse() {
         if (this.puedeCurarse) {
-            const cantidadCuracion = Math.min(30, this.HPmaximo - this.HPactual); // Curar hasta un máximo de 30 HP
+            const cantidadCuracion = Math.min(Math.floor(this.HPmaximo * 0.5), this.HPmaximo - this.HPactual);
             this.HPactual += cantidadCuracion;
-            this.puedeCurarse = false; // Restringir curación a una vez por combate
-            console.log(`${this.nombre} se ha curado ${cantidadCuracion} HP. Ahora tiene ${this.HPactual}/${this.HPmaximo} HP.`);
+            this.puedeCurarse = false;
+            console.log(`${this.nombre} se ha curado ${cantidadCuracion} puntos de HP. Ahora tiene ${this.HPactual}/${this.HPmaximo} HP.`);
         } else {
-            console.log(`${this.nombre} Ya no puede curarse.`);
+            console.log(`${this.nombre} ya no puede curarse en este combate.`);
         }
     }
 }
 
-module.exports=pokemon;
-
+module.exports = pokemon;
